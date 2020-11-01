7f0c84b392d4e2c93580bd438322bc7127534981
centerax <pablos.benitez@gmail.com>
2020-11-01 08:53:08 -0300

    Nueva excepción para validar por Nombre

---
+++ Dominio/Exceptions/YaExisteClienteConEseNombreException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Exceptions
+{
+    public class YaExisteClienteConEseNombreException : Exception
+    {
+        private string message;
+        public override string Message
+        {
+            get { return message; }
+        }
+
+        public YaExisteClienteConEseNombreException()
+        {
+            this.message = "Ya existe un cliente con ese Nombre";
+        }
+    }
+}
