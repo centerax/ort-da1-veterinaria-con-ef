e6c3f26ad0072d5fedf538dc99454293d659bc37
centerax <pablos.benitez@gmail.com>
2020-11-01 08:52:35 -0300

    Agrego archivos necesarios para persistir en SqlServer

---
+++ Dominio/DirectorioSqlServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Dominio.Exceptions;
+
+namespace Dominio
+{
+    public class DirectorioSqlServer
+    {
+        public void AgregarCliente(Cliente unCliente)
+        {
+            ValidarCliente(unCliente);
+            using (VeterinariaContext ctx = new VeterinariaContext())
+            {
+                ctx.Clientes.Add(unCliente);
+                ctx.SaveChanges();
+            }
+        }
+
+        private void ValidarCliente(Cliente unCliente)
+        {
+            using (VeterinariaContext ctx = new VeterinariaContext())
+            {
+                if (ctx.Clientes.Any(c => c.Nombre == unCliente.Nombre))
+                    throw new YaExisteClienteConEseNombreException();
+            }
+        }
+
+        public void AgregarMascota(Mascota unaMascota)
+        {
+            using (VeterinariaContext ctx = new VeterinariaContext())
+            {
+                ctx.Mascotas.Add(unaMascota);
+                ctx.Entry(unaMascota.Duenio).State = EntityState.Unchanged;
+                ctx.SaveChanges();
+            }
+        }
+
+        public List<Cliente> ObtenerClientes()
+        {
+            using (VeterinariaContext ctx = new VeterinariaContext())
+            {
+                return ctx.Clientes.ToList();
+            }
+        }
+    }
+}
---
+++ Dominio/VeterinariaContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class VeterinariaContext : DbContext
+    {
+        public DbSet<Mascota> Mascotas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
